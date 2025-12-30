a92658010d33fc32852e0ed7a3095802c19c49d6
Shady Kuster <82290442+sakuster@users.noreply.github.com>
2025-12-30 09:43:20 -0700

    Add files via upload
    average pcor scripts added

---
+++ 08_partialCorrelation/averagePcor/CHAFdirect_avg-pcor.R
@@ -0,0 +1,76 @@
+### Shady Kuster
+### 9 February 2024
+### Made for the purpose of averaging r^2 values across genes
+
+#load libraries
+library(dplyr)
+library(ggridges)
+library(ggplot2)
+
+setwd("/Users/kusters/Documents/XiphoStartingOver_Aug2024/08_partialCorrelation/averagePcor/CHAF")
+
+#read in the snp file
+snp <- read.table("CHAF_SNPinfo.tsv", header = T, sep = '\t', quote = "") %>%
+  mutate(ID = paste(SNP_scaffold, locus, sep = '.')) %>%
+  filter(class == "direct_n-mt") 
+
+snp$ID <- gsub("-", ".", snp$ID)
+
+#read in the LD file
+ld <- read.table("mt1_results_forLD_directnmts_par1CHAFdata.tsv", 
+                 header = T, sep = '\t', quote = "") %>%
+  mutate(ID = ScaffoldID)
+
+#merge based off of Scaffold ID & locus
+joined_SNPs <- right_join(snp, ld, by = "ID")
+
+#average LD vals based off of gene
+avgLD <- joined_SNPs %>%
+  group_by(attribute) %>%
+  dplyr::summarize(avgP_cor = mean(pcor),
+                   numSamples = sum(samples) / n(),
+                   numAIMs = n_distinct(ID)) 
+
+#export avg LD tsv
+write.table(avgLD, "CHAFdirectnmt_avgpcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+
+#create plot??
+p1 <- ggplot(avgLD, aes(x = attribute, y = avgP_cor)) +
+  geom_point() +
+  theme_classic() +
+  theme(axis.text.x=element_blank(), #remove x axis labels
+        axis.ticks.x=element_blank()) +
+  labs(title = "CHAF Gene Average Partial Correlation", x = "gene")
+
+ggsave("CHAFdirectnmt_avgp-cor.pdf", p1, width = 7, height = 4)
+
+#get top 1% transcripts
+numTranscripts <- length(avgLD$attribute)*0.01
+
+topVals <- as.data.frame(head(avgLD[order(-avgLD$avgP_cor),], n = numTranscripts))
+
+nomTopVals <- right_join(joined_SNPs, topVals, by = "attribute")
+order.scores <- order(-nomTopVals$avgP_cor) 
+top1 <- nomTopVals[order.scores,]
+
+write.table(top1, "CHAFdirectnmt_topOnePercentPcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+
+#get top 5% transcripts
+numTranscripts5 <- round(length(avgLD$attribute)*0.05, 0)
+
+topVals5 <- as.data.frame(head(avgLD[order(-avgLD$avgP_cor),], n = numTranscripts5))
+
+nomTopVals5 <- right_join(joined_SNPs, topVals5, by = "attribute")
+order.scores <- order(-nomTopVals5$avgP_cor) 
+top5 <- nomTopVals5[order.scores,]
+
+write.table(top5, "CHAFdirectnmt_topFivePercentPcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+  
+  
+  
+  
+  
+  
---
+++ 08_partialCorrelation/averagePcor/CHAFnonnmt_AveragePcor.R
@@ -0,0 +1,68 @@
+### Shady Kuster
+### 9 February 2024
+### Made for the purpose of avg LD values (D' or r^2) across genes
+
+#load libraries
+library(dplyr)
+library(ggridges)
+library(dplyr)
+library(ggplot2)
+
+#read in the snp file
+snp <- read.table("CHAF_SNPinfo.tsv", header = T, sep = '\t', quote = "") %>%
+  mutate(ID = paste(SNP_scaffold, locus, sep = '.')) %>%
+  filter(class == "non-n-mt")
+
+snp$ID <- gsub("-", ".", snp$ID)
+
+#read in the LD file
+ld <- read.table("mt1_results_nonnmt_CHAF_ALL.tsv", header = T, sep = '\t', quote = "") %>%
+  mutate(ID = ScaffoldID)
+
+#merge based off of Scaffold ID & locus
+joined_SNPs <- right_join(snp, ld, by = "ID")
+
+#average LD vals based off of gene
+avgLD <- joined_SNPs %>%
+  group_by(attribute) %>%
+  dplyr::summarize(avgP_cor = mean(pcor),
+                   numSamples = sum(samples) / n(),
+                   numAIMs = n_distinct(ID))
+
+#export avg LD tsv
+write.table(avgLD, "CHAFnonnmt_avgpcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+
+#create plot??
+p1 <- ggplot(avgLD, aes(x = attribute, y = avgP_cor)) +
+  geom_point() +
+  theme_classic() +
+  theme(axis.text.x=element_blank()) +
+  labs(title = "CHAF Gene Average R Squared", x = "Gene")
+
+ggsave("CHAFnonnmt_avgpcor.pdf", p1, width = 7, height = 4)
+
+#get top 1% transcripts
+numTranscripts <- length(avgLD$attribute)*0.01
+
+topVals <- as.data.frame(head(avgLD[order(-avgLD$avgP_cor),], n = numTranscripts))
+
+nomTopVals <- right_join(joined_SNPs, topVals, by = "attribute") #can maintain order with inner_join() but doesn't work
+order.scores <- order(-nomTopVals$avgP_cor) 
+top1 <- nomTopVals[order.scores,]
+
+write.table(top1, "CHAFnonnmt_topOnePercentPcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+
+#get top 5% transcripts
+numTranscripts5 <- round(length(avgLD$attribute)*0.05, 0)
+
+topVals5 <- as.data.frame(head(avgLD[order(-avgLD$avgP_cor),], n = numTranscripts5))
+
+nomTopVals5 <- right_join(joined_SNPs, topVals5, by = "attribute")
+order.scores <- order(-nomTopVals5$avgP_cor) 
+top5 <- nomTopVals5[order.scores,]
+
+write.table(top5, "CHAFnonnmt_topFivePercentPcor.tsv", row.names = F, sep = '\t',
+            col.names = T, quote = F)
+
